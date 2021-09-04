44826cae8147239104cd803471f396e454a22d64
RaelViana <catedralviana@hotmail.com>
2021-09-04 12:41:48 -0300

    comando input

---
+++ ex002.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu Nome ? ')  # Cria uma variável que recebe um valor digitado pelo usuário
+print('É um prazer te conhecer, ', nome)  # imprime a mensagem e tambem o valor da variavel 'nome'
+
