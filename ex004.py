225a802f5edb83dca2938097c616b88e9a74b89c
RaelViana <catedralviana@hotmail.com>
2021-09-04 17:26:23 -0300

    Soma entre duas Variáveis

---
+++ ex004.py
@@ -0,0 +1,11 @@
+"""
+    Soma entre dois Números-
+    programa que recebe dois números e efetua a soma entre eles.
+"""
+
+num1 = float(input('Digite o primeiro número: ')) # recebe valor real digitado pelo usuário
+num2 = float(input('Digite o segundo número: '))
+
+s = num1+num2  #  efetua a operação entre as variáveis
+
+print('A soma entre {} e {} é = {}'.format(num1, num2, s)) # imprime o valores digitados e resultado
