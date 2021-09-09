4dd7aee9458e06f56c97d017bf49605159e43669
RaelViana <catedralviana@hotmail.com>
2021-09-09 20:43:38 -0300

    Desafio 05 sucessor e antecessor

---
+++ ex007.py
@@ -0,0 +1,14 @@
+"""
+    Programa que recebe um número e exiba seu sucessor e seu antecessor
+
+"""
+
+
+num = int(input('Digite um número: '))
+
+
+ant = num - 1
+suc = num + 1
+
+
+print('Você digitou: {}  seu sucessor é > {} e o seu Antecessor é > {}'.format(num, suc, ant))
