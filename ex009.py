9dbb280731cc9f4df500770e18f7868a73531b4d
RaelViana <catedralviana@hotmail.com>
2021-09-09 20:53:32 -0300

    Desafio 07 Média Aritmética

---
+++ ex009.py
@@ -0,0 +1,18 @@
+
+"""
+    Programa que recebe duas notas pelo usuário e calcula sua média
+
+"""
+
+
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2)/2
+
+print('A 1°nota foi {}\n A 2° nota foi {}\nA média é  = {}'.format(nota1, nota2, media))
+
+
+
+
+# ----------
