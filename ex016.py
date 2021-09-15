28b6395c07c1a56e9f099befd5d42c644eac5846
RaelViana <catedralviana@hotmail.com>
2021-09-15 11:14:06 -0300

    Desafio 14 Conversor de temperatura

---
+++ ex016.py
@@ -0,0 +1,14 @@
+"""
+    Programa que converte temperaturas
+
+"""
+
+c = float(input('Informe  a temperatura em C°: ')) # Recebe do usuário valor para Graus Celsius
+f = ((9 * c) / 5) + 32   # Converte valor prara Fahrenheit
+
+print('A Temperatura de {:.0f} °C, corresponde a {:.0f} °F !'.format(c, f)) #print do valor resultante
+
+
+
+
+#______________________________________________
