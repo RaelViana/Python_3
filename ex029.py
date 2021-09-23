281c9adc1d799006cd72def79f8322205ba6c87b
RaelViana <catedralviana@hotmail.com>
2021-09-23 19:02:34 -0300

    Procurando uma string dentro de outra

---
+++ ex029.py
@@ -0,0 +1,17 @@
+
+
+"""
+   Programa lê o nome de uma pessoa e verifica se ela
+  tem 'Silva' no nome
+
+"""
+
+nome = str(input('Qual é o seu nome completo: '))
+
+print('Seu nome é {}'.format(nome))
+print('tem silva ? : {}'.format('silva' in nome.lower())) # verifiva a existencia de 'silva' dentro de nome
+
+
+
+
+#-----------------------------------------------------------------------------------
